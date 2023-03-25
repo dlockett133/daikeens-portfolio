a8c38f1791809bc5c9e18a29579866cb1dcd65dc
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 19:07:33 -0400

    created first timeline element

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@material-ui/icons/School'
 
 function Experience() {
   return (
     <div className='experience'> 
-      <VerticalTimeline></VerticalTimeline>
+      <VerticalTimeline lineColor='#3e497a'>
+        <VerticalTimelineElement 
+          className='vertical-timeline-element--education' 
+          date='2011 - 2016'
+          iconStyle={{background: '#3e497a', color: '#fff'}}
+          icon={<SchoolIcon />}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   )
 }
