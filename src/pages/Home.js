0a3f03c48645cc95b86f869e88a631fafae7aa72
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 17:53:02 -0400

    create about me and intro for homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2> Hello World. My Name is Daikeen Lockett</h2>
+        <div className='prompt'>
+          <p>Input details about me here!</p>
+        </div>
+      </div>
+      <div className='skills'></div>
+    </div>
   )
 }
 
