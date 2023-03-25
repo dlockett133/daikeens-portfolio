3808a1f581103c829de90172e983e2f9451bad51
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 17:01:58 -0400

    created navbar button that appears via mobile device, also added logic to make button appear

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import '../styles/Navbar.css';
+import ReorderIcon from '@material-ui/icons/Reorder'
 
 function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
+    const navbarState = () => setExpandNavbar(!expandNavbar);
+
   return (
-    <div className='navbar'>
+    <div className='navbar' id={expandNavbar ? "open" : "close"}>
         <div className='toggleButton'>
-            <button></button>
+            <button> 
+                <ReorderIcon onClick={()=> navbarState()}/> 
+            </button>
         </div>
         <div className='links'>
             {/* Using Link element from 'react-router-dom', you pass a prop call 'to' that you give the route you want the link to go to */}
