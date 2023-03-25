cb830b4c103b4ec094de599fc192432d4f2e9121
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 16:34:04 -0400

    added links to Navbar and imported it to App.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import {Link} from 'react-router-dom';
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='toggleButton'>
+            <button></button>
+        </div>
+        <div className='links'>
+            {/* Using Link element from 'react-router-dom', you pass a prop call 'to' that you give the route you want the link to go to */}
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/experience">Experience</Link>
+        </div>
+    </div>
   )
 }
 
