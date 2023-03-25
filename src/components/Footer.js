f6cfe7e0590d8250e60a0cf77a20f72b062b57c6
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 17:48:34 -0400

    added all icons and links to pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
 import { Twitter } from '@material-ui/icons';
 import { LinkedIn } from '@material-ui/icons';
@@ -8,11 +9,11 @@ import { Email } from '@material-ui/icons';
 function Footer() {
   return (
     <div className='footer'>
-            <div>
-                <Twitter />
-                <LinkedIn />
-                <GitHub />
-                <Email />
+            <div className='socialMedia'>
+                <Link to="https://twitter.com/Lupnthe3rdDay" target="_blank" rel="noopener noreferrer"><Twitter /></Link>
+                <Link to="https://www.linkedin.com/in/daikeen-lockett/" target="_blank" rel="noopener noreferrer"><LinkedIn /></Link>
+                <Link to="https://github.com/dlockett133" target="_blank" rel="noopener noreferrer"><GitHub /></Link>
+                <a href="mailto: daikeen_lockett@yahoo.com"><Email /></a>
             </div>
            <p> &copy; 2023 Daikeen Lockett </p>                 
     </div>
