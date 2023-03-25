bc42ba10af367dcf44ff8babd2bf99dc7a466675
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 17:27:48 -0400

    imported icons to Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Twitter } from '@material-ui/icons';
+import { LinkedIn } from '@material-ui/icons';
+import { GitHub } from '@material-ui/icons';
+import { Email } from '@material-ui/icons';
+
+function Footer() {
+  return (
+    <div className='footer'>
+            <div>
+                <Twitter />
+                <LinkedIn />
+                <GitHub />
+                <Email />
+            </div>
+           <p> &copy; 2023 Daikeen Lockett </p>                 
+    </div>
+  )
+}
+
+export default Footer
