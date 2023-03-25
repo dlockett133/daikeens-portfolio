d3f92667a4809f87ca89f8229682a6ad3fccbdac
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 17:56:59 -0400

    refactored Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
-import { Twitter } from '@material-ui/icons';
-import { LinkedIn } from '@material-ui/icons';
-import { GitHub } from '@material-ui/icons';
-import { Email } from '@material-ui/icons';
+import { Twitter, LinkedIn, GitHub, Email } from '@material-ui/icons';
 
 function Footer() {
   return (
