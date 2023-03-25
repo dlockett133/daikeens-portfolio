d00359517458dbc01a504bc74a1001270f6f9188
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 16:42:51 -0400

    created navbar.css and modified app.css

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           {/* Route tag takes in a path argument that defines the route and the element argument that renders the component when the route is called */}
           <Route path="/" element={<Home/>}/>
