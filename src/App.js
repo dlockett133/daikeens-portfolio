7a92c5d5c9bda53adc6fff0bd2b7e36d3f9c3093
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 16:17:27 -0400

    created routes for portolio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Routes>
+          {/* Route tag takes in a path argument that defines the route and the element argument that renders the component when the route is called */}
+          <Route path="/" element={<Home/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/experience" element={<Experience/>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
