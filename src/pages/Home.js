bfd41c6e29ac193df90361722b5eb4bd8540ac58
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 18:05:53 -0400

    added skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,23 @@ function Home() {
           <GitHub/>
         </div>
       </div>
-      <div className='skills'></div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <ul className='list'>
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>ReactJS, HTML, CSS, NPM, BootStrap</span>
+          </li>
+          <li className='item'>
+            <h2>Back-End</h2>
+            <span>NodeJS, ExpressJS, MySQL, MongoDB</span>
+          </li>
+          <li className='item'>
+            <h2>Languages</h2>
+            <span>JavaScript</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
