36bfae69ef22e2f77e5e6d8f4c2c81d09caada4e
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 18:49:34 -0400

    imported components for react-verticle-timeline

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'> 
+      <VerticalTimeline></VerticalTimeline>
+    </div>
   )
 }
 
