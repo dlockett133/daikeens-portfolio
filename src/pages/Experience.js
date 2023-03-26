c631c6574363f4f550c1391b9527ed3857d65a4c
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-25 20:21:06 -0400

    Created other TimeLine elements and edited the styling

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,20 +2,72 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from '@material-ui/icons/School'
+import WorkIcon from '@material-ui/icons/Work'
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Experience() {
   return (
     <div className='experience'> 
       <VerticalTimeline lineColor='#3e497a'>
+
         <VerticalTimelineElement 
           className='vertical-timeline-element--education' 
-          date='2011 - 2016'
           iconStyle={{background: '#3e497a', color: '#fff'}}
           icon={<SchoolIcon />}
         >
-          <h3 className='vertical-timeline-element-title'> Georgia State University, Atlanta, GA</h3>
+          <h3 className='vertical-timeline-element-title'> Georgia State University</h3>
+          <h4 className='vertical-timeline-element-subtitle'>Atlanta, GA</h4>
           <p>Film/Cinema/Video Studies/Japanese Language</p>
         </VerticalTimelineElement>
+
+        <VerticalTimelineElement 
+          className='vertical-timeline-element--work' 
+          date='2016 - 2021'
+          iconStyle={{background: '#ff8c00', color: '#fff'}}
+          icon={<LibraryMusicIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'> Audio Engineer</h3>
+          <h4 className='vertical-timeline-element-subtitle'>Freelance</h4>
+          <p>
+            As a freelance audio engineer, I utilized industry-standard tools such as ProTools, 
+            Logic Pro X, and Waves plugins to produce high-quality audio for my clients.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement 
+          className='vertical-timeline-element--work' 
+          date='2021 - Present'
+          iconStyle={{background: '#ff8c00', color: '#fff'}}
+          icon={<WorkIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>Calendly</h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            Software Specialist (Atlanta, GA)
+          </h4>
+          <p>
+            As a Software Specialist at Calendly, 
+            I played a crucial role in ensuring our customers had a seamless scheduling 
+            experience by utilizing various tools such as Zendesk, SuperAdmin, Twilio, and SendGrid. 
+            This contributed to Calendly's success and recognition as one of Forbes America's Best Startup Employers 
+            for two consecutive years.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement 
+          className='vertical-timeline-element--education' 
+          date='2022 - 2023'
+          iconStyle={{background: '#3e497a', color: '#fff'}}
+          icon={<SchoolIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'> Georgia Institute of Technology</h3>
+          <h4>Software Engineering Bootcamp Co-hort (Atlanta, GA)</h4>
+          <p>
+            As a software engineering student in Georgia Tech's Bootcamp, 
+            I gained practical experience working with the MERN stack and collaborated on various projects, 
+            developing my skills in full-stack development.
+          </p>
+        </VerticalTimelineElement>
+
       </VerticalTimeline>
     </div>
   )
