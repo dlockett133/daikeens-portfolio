b8d732a26a9d06136d659de015d9449b1b9a8a38
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-27 19:45:55 -0400

    created html to display project based of the id

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
+import { projectList } from '../helpers/ProjectList';
+import GitHubIcon from "@material-ui/icons/GitHub"
 function ProjectDisplay() {
+    const {id} = useParams();
+    const project = projectList[id]
   return (
-    <div>ProjectDisplay</div>
+    <div className='project'>
+        <h1>{project.name}</h1>
+        <img src={project.image} />
+        <GitHubIcon />
+    </div>
   )
 }
 
