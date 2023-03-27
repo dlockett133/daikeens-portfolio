2be18f728fb89a65b907ef3b1422af26735204ff
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-27 18:49:12 -0400

    created assets/images folder

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import ProjectItem from '../components/ProjectItem'
 function Projects() {
   return (
     <div className='projects'>
       <h1>My Projects</h1>
+      <div className='projectList'>
+        <ProjectItem name="WeatherMann" image=""/>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
     </div>
   )
 }
