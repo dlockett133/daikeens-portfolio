10b76f1ea4ff675c37667cb46ce7b116bc318798
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-28 03:04:53 -0400

    modified deployedLinks field

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -7,14 +7,14 @@ export const projectList = [
         image: Project1,
         skills: "HTML, CSS, JavaScript, JQuery, Moment.JS, BootStrap, Server-Side APIs: OpenWeather,UnSplash and Spotify",
         repo: "https://github.com/MontineSproul/the-a-team",
-        deployedLink: "https://diamonddougs.herokuapp.com/"
+        deployedLink: "https://montinesproul.github.io/the-a-team/"
     },
     {
         name: "weatherOrNot",
         image: Project2,
         skills: "HTML, CSS, JavaScript, BootStrap, Node, Express, Sequelize, JawsDB, MySQL, Heroku, Server-Side APIs: OpenWeather",
         repo: "https://github.com/maxxAbow/weatherOrNot",
-        deployedLink: "https://montinesproul.github.io/the-a-team/"
+        deployedLink: "https://diamonddougs.herokuapp.com/"
 
     }
 ];
