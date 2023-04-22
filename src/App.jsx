0b1cdfbbb4a5b50bb4bda45d88f15cacc062351a
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-22 00:09:44 -0400

    inserted components to App.

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,28 @@ import {
   Tech, 
   Works,
   StarsCanvas } from "./components";
-  
+
 BrowserRouter
 function App() {
 
   return (
-      <div>   
-        3D Developer Portfolio
+      <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero/>
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
+      </BrowserRouter>
   )
 }
 
