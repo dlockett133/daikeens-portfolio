206e5ae8d7f4ea0cc203421a50cab2a91587369a
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-23 18:07:53 -0400

    Added technologies

--- src/constants/index.js
+++ src/constants/index.js
@@ -71,20 +71,8 @@ import {
       icon: javascript,
     },
     {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
       name: "React JS",
       icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
     },
     {
       name: "Node JS",
@@ -97,18 +85,6 @@ import {
     {
       name: "Three JS",
       icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
     },
   ];
   
